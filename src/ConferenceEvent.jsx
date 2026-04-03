5fa002f17bc85337c125b46717e350cebe053b66
vzz-dev <workxoff@gmail.com>
2026-04-03 13:20:46 -0400

    proyecto culminado, All is ok

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -88,44 +88,43 @@ const ConferenceEvent = () => {
 
 
     const ItemsDisplay = ({ items }) => {
-        const ItemsDisplay = ({ items }) => {
-            console.log(items);
-            return <>
-                <div className="display_box1">
-                    {items.length === 0 && <p>No items selected</p>}
-                    <table className="table_item_data">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Unit Cost</th>
-                                <th>Quantity</th>
-                                <th>Subtotal</th>
+        console.log(items);
+        return <>
+            <div className="display_box1">
+                {items.length === 0 && <p>No items selected</p>}
+                <table className="table_item_data">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Unit Cost</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>${item.cost}</td>
+                                <td>
+                                    {item.type === "meals" || item.numberOfPeople
+                                    ? ` For ${numberOfPeople} people`
+                                    : item.quantity}
+                                </td>
+                                <td>{item.type === "meals" || item.numberOfPeople
+                                    ? `${item.cost * numberOfPeople}`
+                                    : `${item.cost * item.quantity}`}
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {items.map((item, index) => (
-                                <tr key={index}>
-                                    <td>{item.name}</td>
-                                    <td>${item.cost}</td>
-                                    <td>
-                                        {item.type === "meals" || item.numberOfPeople
-                                        ? ` For ${numberOfPeople} people`
-                                        : item.quantity}
-                                    </td>
-                                    <td>{item.type === "meals" || item.numberOfPeople
-                                        ? `${item.cost * numberOfPeople}`
-                                        : `${item.cost * item.quantity}`}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </>
-        };
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    };
+    
         
 
-    };
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
@@ -163,7 +162,7 @@ const ConferenceEvent = () => {
             setShowItems(!showItems); // Toggle showItems to true only if it's currently false
           }
         }
-      }
+    }
 
     return (
         <>
@@ -346,5 +345,4 @@ const ConferenceEvent = () => {
 
     );
 };
-
 export default ConferenceEvent;
